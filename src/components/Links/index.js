06db8f9104b193b178b5414f3b1e9e429e33c011
Vanity <ktkyletran@gmail.com>
2022-01-16 14:25:48 -0600

    added endpoint links to navbar

---
+++ src/components/Links/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const links = [
+  { url: '/search', text: 'All' },
+  { url: '/news', text: 'News' },
+  { url: '/images', text: 'Images' },
+  { url: '/videos', text: 'Videos' },
+]
+const Links = () => {
+  return (
+    <div className='flex sm:justify-start justify-start items-center mt-4'>
+      {links.map(({url, text}) => (
+        <NavLink to={url} className={({ isActive }) => (isActive ? 'text-blue-700 border-b-2 dark:text-blue-300 border-blue-700 pb-2 m-2 mb-0': ' m-2 mb-0')}>
+          {text}
+        </NavLink>
+      ))}
+    </div>
+  )
+}
+
+export default Links
